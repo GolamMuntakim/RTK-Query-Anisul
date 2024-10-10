a9ba39467ecc812f50e5610c1b030c855fd5ba12
Golam Muntakim <seams123456789@gmail.com>
2024-10-09 17:39:48 -0700

    data fetching done

--- src/features/products/ProductView.jsx
+++ src/features/products/ProductView.jsx
@@ -1,7 +1,24 @@
+import { useGetProductsQuery } from "../../services/productsApi";
+
 const ProductView = () => {
+   const {data:products, isLoading, isFetching, isSuccess,error} =  useGetProductsQuery()
+   console.log(products)
+   
     return (
-        <div>
-            List of products
+        <div className="w-full h-screen">
+           <h1 className="font-bold">List of products</h1>
+           <p className="content-center text-center">{isLoading && <p>Loading</p> }</p>
+           <p className="content-center text-center">{error && <p>{error.message}</p>}</p>
+           {!isLoading && !error && products && products.length>0 && (
+                 <div className="products">
+                   { products.map((product)=>{
+                    return <div key={product?.id}>
+                        <h1 className="block"><span className="font-bold">Title :</span> {product?.title}</h1>
+                    </div>
+                   })}
+                 </div>
+            )}
+          
         </div>
     );
 };
